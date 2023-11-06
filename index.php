131100c1bc56a4a3b61fcf1786cbceb55eb68413
Jonas-than <jonathanlitardo48@gmail.com>
2023-11-06 16:59:35 -0600

    installing material UI and making signing

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+    <title>Sign-Up</title>
+</head>
+<body>
+<main>
+    <div>
+        <div>
+            <div>
+            <h2>Join thousands of learners from around the world</h2>
+            <span>Master web development by making real-life projects.
+                There are multiple paths for you to choose
+            </span>
+            </div>
+            <div>
+                <form action="./views/personal_info.php" method="post">
+                    <input type="email" name="email" placeholder="Email">
+                    <input type="password" name="password" placeholder="Password">
+                    <button>Start coding now</button>
+                </form>
+            </div>
+            <div>
+                <span>or continue with these social profile</span>
+                <img src="./assets/Google.svg" alt="">
+                <img src="./assets/Facebook.svg" alt="">
+                <img src="./assets/Twitter.svg" alt="">
+                <img src="./assets/Gihub.svg" alt="">
+                <span>Already a member?<a href="">Login</a></span>
+            </div>
+        </div>
+    </div>
+</main>
+
+    
+</body>
+</html>
